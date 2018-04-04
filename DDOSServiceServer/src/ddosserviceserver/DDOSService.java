25e5fc2091c3562f2ddc9443d3a73fe6a715db1f
Atakltihaileslase <38071630+Atakltihaileslase@users.noreply.github.com>
2018-04-04 04:21:08 -0700

    Add files via upload

---
+++ DDOSServiceServer/src/ddosserviceserver/DDOSService.java
@@ -0,0 +1,9 @@
+
+package ddosserviceserver;
+import java.rmi.*;
+// DDOSService Interface
+// Interface for a RMI service that will actually attack on a target machine
+public interface DDOSService extends java.rmi.Remote{
+  public String attack ( )
+throws RemoteException;  
+}
