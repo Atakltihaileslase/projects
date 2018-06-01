4bc32cbde3f6d5f941458a602e2a3eb1e57dcb4e
Atakltihaileslase <38071630+Atakltihaileslase@users.noreply.github.com>
2018-06-01 11:43:46 -0700

    Update chat_server.c

--- Message Passing Interfaces/chat_server.c
+++ Message Passing Interfaces/chat_server.c
@@ -9,14 +9,14 @@
 int main() {
 
     int shm_id;
-    size_t size = 200;
-    int shmflg =  IPC_CREAT | 0666; // read write permission for all users
+    size_t size = 300;
+    int shm_flg =  IPC_CREAT | 0666; // read write permission for all users
 
     key_t key = 12345;
     
-    char *message, *head;
+    char *msg, *chars;
 
-    shm_id = shmget(key, size, shmflg);
+    shm_id = shmget(key, size, shm_flg);
 
     if(shm_id == -1) {
         perror("Error: shmget has failed\n");
@@ -24,42 +24,42 @@ int main() {
     }
 
     // attached shared memory to the process's address space
-    head = shmat(shm_id, NULL, 0);
+    chars = shmat(shm_id, NULL, 0);
 
-    if(head == (char*)-1) {
+    if(chars == (char*)-1) {
         perror("ERROR: shmat has failed \n");
         exit(0);
     }
 
 	// reserve the first char for flagging purpose
-	message = head + 1;
+	msg = chars + 1;
 
 	while (TRUE) {
 
 		// wait for a reply
-		while(head[0] != 'y') {
+		while(chars[0] != 'y') {
 		    sleep(1);
 		}
 		
 		// set the not ready flag
-		head[0] = 'n';
+		chars[0] = 'n';
 
 		// print the friend's reply
-		printf("Friend >>> %s", message);
+		printf("the shared friend >>> %s",msg);
 
 		// print the prompt 
-		printf("You >>> ");
+		printf("I am >>> ");
 
 		// get input message
-		fgets(message, size - 2, stdin);
+		fgets(msg, size - 2, stdin);
 
 		// set flag in read state
-		head[0] = 'f';
+		chars[0] = 'f';
 
 	}
 
-    // and detach the shared memory
-    if(shmdt(head) == -1) {
+    // detach the shared memory
+    if(shmdt(chars) == -1) {
         perror("ERROR: detaching shared memory failed");
     }
 
