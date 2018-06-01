daa2549a7445ccbbe96498fc2eee587d1fff94d3
Atakltihaileslase <38071630+Atakltihaileslase@users.noreply.github.com>
2018-06-01 11:14:20 -0700

    Update runner.cpp

--- C++HeaderFile/runner.cpp
+++ C++HeaderFile/runner.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int main() {
 
-    Student student("ATR/2100/08");
-    student.setFirstName("Elias");
-    student.setLastName("Addisu");
+    Student student("ATR/0526/08");
+    student.setFirstName("Ataklti");
+    student.setLastName("Haileslase");
     student.setDepartment("Software Engineering");
-    student.setClassYear("Junior");
+    student.setClassYear("seniour");
 
     student.displayStudentInfo();
 
